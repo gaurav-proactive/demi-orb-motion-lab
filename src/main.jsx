6bdc7e2934dff0a5c904f393e13ea2ac74395e4b
Gaurav Bisen <gaurav@proactivelabs.ai>
2026-08-04 17:35:19 -0700

    Synchronize orb system routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import { CognitionPage } from "./CognitionPage.jsx";
 import "./styles.css";
 
-const normalizedPath = window.location.pathname.replace(/\/+$/, "");
-const isCognitionPage = normalizedPath.endsWith("/cognition") || window.location.hash === "#/cognition";
+function isCognitionRoute() {
+  const normalizedPath = window.location.pathname.replace(/\/+$/, "");
+  return normalizedPath.endsWith("/cognition") || window.location.hash === "#/cognition";
+}
+
+function OrbSystemRouter() {
+  const [isCognitionPage, setIsCognitionPage] = useState(isCognitionRoute);
+
+  useEffect(() => {
+    const syncRoute = () => setIsCognitionPage(isCognitionRoute());
+    window.addEventListener("hashchange", syncRoute);
+    window.addEventListener("popstate", syncRoute);
+    return () => {
+      window.removeEventListener("hashchange", syncRoute);
+      window.removeEventListener("popstate", syncRoute);
+    };
+  }, []);
+
+  return isCognitionPage ? <CognitionPage /> : <App />;
+}
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isCognitionPage ? <CognitionPage /> : <App />}
+    <OrbSystemRouter />
   </React.StrictMode>,
 );
